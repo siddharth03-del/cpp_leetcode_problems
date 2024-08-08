f7e477751a36d4a3e46938f48b43c06c38d82ad6
siddharth03-del <148877613+siddharth03-del@users.noreply.github.com>
2024-08-08 19:35:18 +0530

    Leetcode Solutions

---
+++ Make_two_arrays_equal_by_reversing_subarray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution{
+    public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr){
+        sort(arr.begin(), arr.end());
+        sort(target.begin(), target.end());
+        for(int i = 0; i < target.size(); i++){
+            if(arr[i] != target[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
